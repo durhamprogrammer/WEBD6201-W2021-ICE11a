f8a66198109485fa5489ceb2e608f124a560bd5d
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-04-06 09:15:36 -0400

    added auth support via passport

--- Server/Config/app.ts
+++ Server/Config/app.ts
@@ -6,6 +6,18 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import mongoose from 'mongoose';
 
+// modules for authentication
+import session from 'express-session';
+import passport from 'passport';
+import passportLocal from 'passport-local';
+
+// authentication objects
+let localStrategy = passportLocal.Strategy; // alias
+import User from '../Models/user';
+
+// module for auth messaging and error management
+import flash from 'connect-flash';
+
 // App configuration
 import indexRouter from '../Routes/index';
 const app = express();
@@ -32,6 +44,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../../Client/')));
 app.use(express.static(path.join(__dirname, '../../node_modules/')));
+
+// setup express session
+app.use(session({
+  secret: DBConfig.Secret,
+  saveUninitialized: false,
+  resave: false
+}));
+
+// initialize flash
+app.use(flash());
+
+// initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// implement an Auth Strategy
+passport.use(User.createStrategy());
+
+// serialize and deserialize the user data
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use('/', indexRouter);
 
